c00def67b3beec5700b8bc464f41ae9fd1b4f332
unknown <bharathkumar100q@gmail.com>
2024-09-17 00:03:28 -0700

    six30labs

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,9 +1,20 @@
-
+import Locations from "../../components/Locations/Locations";
+import map from "../../assets/contacts/map.png";
 
 function Contacts() {
   return (
-    <div>Contacts</div>
-  )
+    <>
+      <div
+        className="flex justify-center items-center flex-col mt-14"
+        style={{ backgroundImage: `url(${map})` }}
+      >
+        <Locations />
+      </div>
+      <div className="flex flex-1 items-center ml-20 mt-20">
+        <h2>If you have any queries please feel free to leave us a message</h2>
+      </div>
+    </>
+  );
 }
 
-export default Contacts
+export default Contacts;
