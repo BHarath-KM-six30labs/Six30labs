07d7dd9b4f6dcc7124da1867c70a014c7f1fed1b
unknown <bharathkumar100q@gmail.com>
2024-09-19 04:21:36 -0700

    responsive started

--- src/pages/Home/Sections/Portfolio.tsx
+++ src/pages/Home/Sections/Portfolio.tsx
@@ -14,7 +14,7 @@ function Portfolio() {
             <Card key={index} project={project} show={false} />
           ))}
         </div>
-        <h3 className="text-xl sm:text-2xl font-semibold mt-10">ACCELERATORS</h3>
+        {/* <h3 className="text-xl sm:text-2xl font-semibold mt-10">ACCELERATORS</h3> */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-8">
           {accelerators.map((project, index) => (
             <Card key={index} project={project} show={false} />
