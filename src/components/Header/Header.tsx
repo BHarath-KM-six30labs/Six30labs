6b24982ec2ce4afde3ee1058cdc69e82df6d91b6
unknown <bharathkumar100q@gmail.com>
2024-09-20 04:52:15 -0700

    animation started

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -260,7 +260,7 @@ function Header() {
                          initial={{ opacity: 0, y: -20 }} 
                          animate={{ opacity: 1, y: 0 }} 
                          exit={{ opacity: 0, y: -20 }} 
-                         transition={{ duration: 0.2 }} 
+                         transition={{ duration: 0.3 }} 
                       >
                         <MobileMenuItem
                           to="/software_development"
